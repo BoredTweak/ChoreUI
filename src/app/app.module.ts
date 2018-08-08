5b9687a20cb54b7aba42b3501e7b19b24d1a5962
Alex Elia <alex@coderpages.com>
2018-08-08 10:14:57 -0700

    initial commit of chore ui angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './@shared/header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './@shared/footer/footer.component';
 
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { AppRouting } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
+    AppRouting,
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
